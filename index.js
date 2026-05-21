313a096d3eba6cca8ccdb30983f40a0b3e21343b
Bilal Ahmad Shaheen <bilaiahmad.shaheen@gmail.com>
2026-05-21 16:33:30 +0500

    Updated cors origin for live setup

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT
 
 const app = express()
 
-app.use(cors({origin: 'http://localhost:5173',credentials: true}))
+app.use(cors({origin: true,credentials: true}))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
